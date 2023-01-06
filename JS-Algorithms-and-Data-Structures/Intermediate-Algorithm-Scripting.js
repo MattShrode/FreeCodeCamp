f8e0e1b51e1084d4085041c30b039b150df09513
Matt Shrode <mattshrode@gmail.com>
2023-01-05 21:59:28 -0600

    Intermediate Algorithm Scripting
    Continue working on the exercies in the intermediate algorithm
    scripting section.

--- JS-Algorithms-and-Data-Structures/Intermediate-Algorithm-Scripting.js
+++ JS-Algorithms-and-Data-Structures/Intermediate-Algorithm-Scripting.js
@@ -31,3 +31,65 @@
 
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 }
+
+//Seek and Destroy
+{
+  function destroyer(arr) {
+    let args = [...arguments].slice(1);
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      let remove = false;
+      for (let j = 0; j < args.length; j++) {
+        if (arr[i] === args[j]) {
+          remove = true;
+        }
+      }
+      if (!remove) {
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
+  }
+
+  destroyer([2, 3, 2, 3], 2, 3);
+}
+
+//Wherefore Art Thou
+{
+  function whatIsInAName(collection, source) {
+    const keys1 = Object.keys(source);
+    return collection.filter((obj) => {
+      for (let i = 0; i < keys1.length; i++) {
+        if (
+          !obj.hasOwnProperty(keys1[i]) ||
+          obj[keys1[i]] !== source[keys1[i]]
+        ) {
+          return false;
+        }
+      }
+      return true;
+    });
+  }
+
+  whatIsInAName(
+    [
+      { first: "Romeo", last: "Montague" },
+      { first: "Mercutio", last: null },
+      { first: "Tybalt", last: "Capulet" },
+    ],
+    { last: "Capulet" }
+  );
+}
+
+//Spinal Tap Case
+{
+  function spinalCase(str) {
+    let newStr = str
+      .split(/(?<=[a-z])(?=[A-Z])|\W|[_]/g)
+      .join("-")
+      .toLowerCase();
+    return newStr;
+  }
+
+  spinalCase("AllThe-small Things");
+}
