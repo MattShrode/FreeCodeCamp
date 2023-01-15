a06ec039eb4cc9fc0851ad39ca2dc856383bcb6c
Matt Shrode <mattshrode@gmail.com>
2023-01-14 23:06:06 -0600

    Cash Register
    Work on the cash register project.

--- JS-Algorithms-and-Data-Structures/Projects/Cash-Register.js
+++ JS-Algorithms-and-Data-Structures/Projects/Cash-Register.js
@@ -19,20 +19,42 @@ function checkCashRegister(price, cash, cid) {
     }
     return object;
   }
-  let change;
-  return change;
+  /*let modifiedDrawer = [];
+    for (const element of cid) {
+      modifiedDrawer.push(element);
+    }
+    modifiedDrawer.forEach(element => {
+      for(let i=1; i<element.length; i++) {
+        element[i] = Math.round(element[i] * 100);
+      }
+    });*/
+  console.log(changeDue);
+  const denomination = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+  if (totalcid > changeDue) {
+    for (let i = cid.length - 1; i >= 0; i--) {
+      if (changeDue >= cid[i][1] && cid[i][1] !== 0) {
+        object.change.push(cid[i]);
+        changeDue -= cid[i][1];
+      } else if (changeDue < cid[i][1]) {
+        let difference = cid[i][1] - changeDue;
+        console.log(cid[i], difference);
+      }
+    }
+    console.log(changeDue);
+  }
+  return object;
 }
 
 console.log(
-  checkCashRegister(19.5, 20, [
-    ["PENNY", 0.5],
-    ["NICKEL", 0],
-    ["DIME", 0],
-    ["QUARTER", 0],
-    ["ONE", 0],
-    ["FIVE", 0],
-    ["TEN", 0],
-    ["TWENTY", 0],
-    ["ONE HUNDRED", 0],
+  checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
   ])
 );
